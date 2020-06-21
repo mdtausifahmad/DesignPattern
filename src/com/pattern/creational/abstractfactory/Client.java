6a537085b087a5fe0f52455a987d49d7727e8d01
MD Tausif Ahmad <md.ahmad@emaratech.ae>
2020-06-21 11:32:16 +0400

    initial commit

---
+++ src/com/pattern/creational/abstractfactory/Client.java
@@ -0,0 +1,9 @@
+package com.pattern.creational.abstractfactory;
+
+public class Client {
+
+    public static void main(String[] args) {
+        ComputerA computer1 = new ComputerA();
+
+    }
+}
---
+++ src/com/pattern/creational/abstractfactory/ComputerA.java
@@ -0,0 +1,4 @@
+package com.pattern.creational.abstractfactory;
+
+public class ComputerA {
+}
